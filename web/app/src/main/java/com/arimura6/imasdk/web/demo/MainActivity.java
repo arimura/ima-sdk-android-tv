394c8a851efd98c63df863d2cb39a4455cfd829a
arimura <33759663+arimura@users.noreply.github.com>
2024-04-01 11:51:04 +0900

    Add webview

--- web/app/src/main/java/com/arimura6/imasdk/web/demo/MainActivity.java
+++ web/app/src/main/java/com/arimura6/imasdk/web/demo/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends FragmentActivity {
 
@@ -11,5 +12,8 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("https://www.google.com");
     }
 }
