455d69d4dfcf6895f3025748bc4bb5973aa0bcf9
arimura <33759663+arimura@users.noreply.github.com>
2024-04-01 11:33:13 +0900

    Add web

---
+++ web/app/src/main/java/com/arimura6/imasdk/web/demo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.arimura6.imasdk.web.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
