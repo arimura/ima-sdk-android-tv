08d525070d14eb83d7b6af720373b89a6cfd0eb5
arimura <33759663+arimura@users.noreply.github.com>
2024-04-02 10:19:22 +0900

    ...

--- web/app/src/main/java/com/arimura6/imasdk/web/demo/MainActivity.java
+++ web/app/src/main/java/com/arimura6/imasdk/web/demo/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.ConsoleMessage;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 
 public class MainActivity extends FragmentActivity {
@@ -13,8 +16,25 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        WebView.setWebContentsDebuggingEnabled(true);
+
         WebView myWebView = (WebView) findViewById(R.id.webview);
-        myWebView.getSettings().getJavaScriptEnabled();
+        myWebView.getSettings().setJavaScriptEnabled(true);
+
+        myWebView.setWebChromeClient(new WebChromeClient() {
+//            @Override
+//            public void onConsoleMessage(String message, int lineNumber, String sourceID) {
+//                Log.d("WebView", message + " -- From line " + lineNumber + " of " + sourceID);
+//            }
+
+            @Override
+            public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+                Log.d("WebView", consoleMessage.message() + " -- From line "
+                        + consoleMessage.lineNumber() + " of " + consoleMessage.sourceId());
+                return true;
+            }
+        });
+
         myWebView.loadUrl("file:///android_asset/index.html");
     }
 }
