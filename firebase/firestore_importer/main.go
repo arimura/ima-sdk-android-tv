b1fafa4d3ca19be572694ac3ffb741164b47b44e
arimura <33759663+arimura@users.noreply.github.com>
2024-04-11 17:37:53 +0900

    Update based on JSON

--- firebase/firestore_importer/main.go
+++ firebase/firestore_importer/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
+	"io"
 	"log"
+	"os"
 	"time"
 
 	"cloud.google.com/go/firestore"
@@ -11,10 +14,33 @@ import (
 	"google.golang.org/api/option"
 )
 
+type signage struct {
+	Name      string   `json:"name"`
+	Endpoints []string `json:"endpoints"`
+}
+
 func main() {
 	serviceAccountKeyFilePath := flag.String("cred", "", "The file path to the service account key json file.")
+	signageFilePath := flag.String("signage", "", "The file path to the signage json file.")
 	projectID := flag.String("projectID", "", "The project ID.")
 	flag.Parse()
+
+	jsonFile, err := os.Open(*signageFilePath)
+	if err != nil {
+		log.Fatalf("error opening JSON file: %v\n", err)
+	}
+	defer jsonFile.Close()
+
+	byteValue, err := io.ReadAll(jsonFile)
+	if err != nil {
+		log.Fatalf("error reading JSON file: %v\n", err)
+	}
+
+	var signages map[string]signage
+	err = json.Unmarshal(byteValue, &signages)
+	if err != nil {
+		log.Fatalf("error unmarshalling JSON data: %v\n", err)
+	}
 
 	opt := option.WithCredentialsFile(*serviceAccountKeyFilePath)
 	config := &firebase.Config{ProjectID: *projectID}
@@ -28,21 +54,22 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	addDocAsMap(ctx, client)
+	addDocAsMap(ctx, client, signages)
 	defer client.Close()
 
 }
 
-func addDocAsMap(ctx context.Context, client *firestore.Client) error {
-	_, err := client.Collection("test").Doc("hoge").Set(ctx, map[string]interface{}{
-		"name":      "Los Angeles",
-		"state":     "CA",
-		"country":   "USA",
-		"updatedAd": time.Now(),
-	})
-	if err != nil {
-		log.Printf("An error has occurred: %s", err)
+func addDocAsMap(ctx context.Context, client *firestore.Client, siganes map[string]signage) error {
+	for id, signage := range siganes {
+		_, err := client.Collection("test").Doc(id).Set(ctx, map[string]interface{}{
+			"name":      signage.Name,
+			"endpoints": signage.Endpoints,
+			"updatedAd": time.Now(),
+		})
+		if err != nil {
+			log.Printf("An error has occurred: %s", err)
+			return err
+		}
 	}
-
-	return err
+	return nil
 }
